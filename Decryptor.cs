1af03a62a6f56266482e153020c544b9830826d3
nullinghax <107588666+nullinghax@users.noreply.github.com>
2022-11-02 07:51:23 -0400

    only god understands this code now

---
+++ Decryptor.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+
+namespace ArmDot {
+    internal class Decryptor {
+        public static int Init(ModuleDefMD Module) {
+            int count = 0;
+
+            foreach(var type in Module.Types) {
+                foreach(var method in type.Methods.Where(x => !x.IsNative && x.HasBody)) {
+                    var body = method.Body;
+                    var instrs = body.Instructions;
+                    int startingIndex = 0;
+                    int endingIndex = 0;
+                    for(int i=0;i<instrs.Count;i++) {
+                        var instr = instrs[i];
+                        startingIndex = i;
+                        if(instr.OpCode == OpCodes.Ldc_I4) {
+                            if(instrs[i+1].OpCode == OpCodes.Newarr) {
+                                bool isChar = instrs[i + 1].Operand.ToString().Contains("Char");
+                                if(isChar) {
+                                    var times = instr.GetLdcI4Value();
+                                    int first = 0;
+                                    char[] chars = new char[times];
+                                    i += 5;
+                                    for(var j = 0; j < times; j++) {
+                                        chars[j] = (char)(instrs[i + 1].GetLdcI4Value() ^ instrs[i + 2].GetLdcI4Value());
+                                        if(j == times) {
+                                            endingIndex = instrs.IndexOf(instrs[i + 5]);
+                                            
+                                            break;
+                                        } else { i += 6; }
+                                    }
+                                    var obj = instrs.First(x => x.OpCode == OpCodes.Newobj && x.Operand.ToString().Contains("String"));
+                                    var lol = instrs.IndexOf(obj);
+                                    if(instrs[lol-1].OpCode == OpCodes.Stelem_I2 && instrs[lol - 2].OpCode == OpCodes.Xor) {
+                                        obj.OpCode = OpCodes.Ldstr;
+                                        obj.Operand = new string(chars);
+                                    }
+                                    while(startingIndex < lol) {
+                                        instrs[startingIndex].OpCode = OpCodes.Nop;
+                                        startingIndex++;
+                                    }
+
+                                }
+                            }
+                        }
+                    }
+                    
+                    
+
+                }
+                
+            }
+
+
+            return count;
+        }
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using dnlib.DotNet;
+using dnlib.DotNet.Writer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmDot {
+    internal class Program {
+        static void Main(string[] args) {
+            string path = args[0];
+            var module = ModuleDefMD.Load(path);
+            Decryptor.Init(module);
+            if(module.IsILOnly) {
+                ModuleWriterOptions options = new ModuleWriterOptions(module);
+                options.Logger = DummyLogger.NoThrowInstance;
+                module.Write(path.Replace(".exe", "-deobf.exe"), options);
+                return;
+            }
+            NativeModuleWriterOptions writer = new NativeModuleWriterOptions(module, false);
+            writer.Logger = DummyLogger.NoThrowInstance;
+            module.NativeWrite(path.Replace(".exe", "-deobf.exe"), writer);
+        }
+    }
+}
